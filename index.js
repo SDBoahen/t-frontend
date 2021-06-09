1994cadab5df3ca3e4a024828326eb0aafa70c33
SDBoahen <SDBoahen@gmail.com>
2021-06-09 09:24:13 -0400

    GO

---
+++ index.js
@@ -0,0 +1,3 @@
+document.addEventListener("click", (event)=>{ console.log("💻🔬👀:: You Just Clicked on == ", event.target) }  )
+////  'Listen To TheDOM🌌🧘🌠👁✨'
+document.addEventListener("DOMContentLoaded", function(){ /* */ console.log("WE, ARE,, LIVE🙆🏾‍♂️✨") })
